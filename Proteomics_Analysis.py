4551be051b3872937da4c163d6cc9b2cc014d66b
ablancsong <92765325+ablancsong@users.noreply.github.com>
2023-10-23 15:43:44 +0900

    Update Proteomics_Analysis.py

--- Proteomics_Analysis.py
+++ Proteomics_Analysis.py
@@ -1,1 +1,22 @@
 https://github.com/Nesvilab/MSFragger
+
+Building from scratch
+Update build version:
+The version of the build is stored in 3 separate places:
+
+File: MSFragger-GUI/src/umich/msfragger/gui/Bundle.properties
+Property: msfragger.gui.version
+File: MSFragger-GUI/build.gradle
+Property: version
+File: MSFragger-GUI/src/umich/msfragger/gui/Bundle.properties 
+Property: msfragger.gui.version
+Build:
+You don't need to have Gradle installed, the Gradle wrapper included in this repository will be used. From the root directory of the repository issue the following commands:
+
+cd ./MSFragger-GUI
+./gradlew makeReleaseZipNoJre
+or use this version to build with Java Runtime (for Windows only):
+
+cd ./MSFragger-GUI
+./gradlew makeReleaseZipWithJre
+The .zip output will be in MSFragger-GUI/build/github-release.
